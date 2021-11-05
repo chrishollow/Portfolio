e05bad1db02bee68dbf33d3e6900aa77328c4b38
chrishollow <christian_bokoto@hotmail.com>
2021-11-05 13:24:02 +0100

    Intro

--- pages/homepage.php
+++ pages/homepage.php
@@ -11,7 +11,14 @@
             <?php include "menu.php";?>
 
         <main>
-            <h1>Christian BOKOTO</h1>
+            <article>
+            <h1>Qui suis-je ? Que fais-je ? Où vais-je ?</h1>
+            
+            <p>En 2021 ma volonté de vouloir développer des compétences dans le domaine du digital, s'est soldée par une décision d'entreprendre une formation en développement web.
+            J'aimerais être un genre de codeur polyvalent, tant dans la partie structural que dans le design.</p>
+            <p>Après cette formation, mon projet serait de pouvoir louer mes compétences en tant que développeur web à mes amis et connaissances qui ont un business ou une passion. Lorsque j’arriverai à créer et à entretenir correctement un site web, je postulerai pour un emploi en entreprise.</p>
+            <p>Dans l'avenir, j’aimerais travailler dans une entreprise innovante, créative et inclusive, où la diversité de mes compétences de web developer peuvent être mises à service. Une entreprise où j’aurai une fonction polyvalente avec la possibilité de la voir évoluer au sein de la société. </p>
+            </article>
             
         </main>
 
